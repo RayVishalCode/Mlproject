9b241c8da878d3a77f5f4ece5bbe7d8c2d263baa
Vishal Ray <vishalray464407@gmail.com>
2025-10-30 01:04:08 +0530

    data ingestion done

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from src.logger import logging
-from src.exception import CustomException
-import sys  
+# from src.logger import logging
+# from src.exception import CustomException
+# import sys  
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by zero error")
-        raise CustomException(e,sys)
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divided by zero error")
+#         raise CustomException(e,sys)
